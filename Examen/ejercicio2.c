5a4444ba38dab557ae54acfa801d4e0034529c33
EmanuelLp <pa3z2000@gmail.com>
2021-05-19 09:47:15 -0500

    ecamen

--- Examen/ejercicio2.c
+++ Examen/ejercicio2.c
@@ -12,10 +12,32 @@ do{
 for(i=0;i<5;i++){
 	printf("%c\n",arrayChar[i]);
 }*/
+
+char *punteroChar;
+punteroChar=&arrayChar[0];
+	
 char *punteroCharDos;
 punteroCharDos=&arrayChar[0];
-printf("%c\n",arrayChar[0]);
-printf("\nEL contenido del puntero es: %c",*punteroCharDos);
-printf("%c\n",arrayChar[5]);
+
+printf("\nArray primer valor: %c",arrayChar[0]);
+printf("\nPrimer elemento del punteroChar: %c",*punteroChar);
+printf("\nPrimer elemento del pubteroCharDos: %c",*punteroCharDos);	
+printf("\nQuinto elemento del array: %c",arrayChar[4]);	
+printf("\nQuinto elemento del punteroChar: %c",*(punteroChar+4));
+printf("\nQuinto elemento del punteroCharDos: %c",*(punteroCharDos+4));
+int i;
+printf("\n11.-PunteroCharDos ");
+for (i = 0; i < 5; ++i){
+	printf("\nLetra: %c", *(punteroCharDos+i));
+}
+printf("\n12.-PunteroCharDos ");
+for (i = 0; i < 5; ++i){
+	printf("\nLetra: %c", *(punteroCharDos+i)); 
+}
+/*printf("\n-PunteroChar ");
+for (i = 0; i < 5; ++i){
+	printf("\nLetra: %c", *(punteroChar+i)); 
+}*/
+
 return 0;
 }
